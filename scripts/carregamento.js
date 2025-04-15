7f42828b9dbab4c70ae9e2b370cc3faeed4d780f
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-04-14 22:01:54 -0300

    Links do JS funcionam apenas no GitHub Pages

--- scripts/carregamento.js
+++ scripts/carregamento.js
@@ -1,5 +1,5 @@
 setTimeout(redirecionar, 1000);
 
 function redirecionar(){
-    window.location.href = '../../the_train/login/login.html';
+    window.location.href = '../the_train/login/login.html';
 }
