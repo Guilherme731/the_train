671d1c0c2350326695fa2eddb0772c95f436ec1b
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-05-13 12:47:55 -0300

    Alteração do tamanho do gráfico de linhas.

--- scripts/controleGraficoLinhas.js
+++ scripts/controleGraficoLinhas.js
@@ -4,9 +4,9 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Mes', 'Trem 1', 'Trem 2', 'Trem 3'],
-    ['2004',  90,       40,        30],
-    ['2005',  95,       70,        80],
-    ['2006',  87,       89,        70]
+    ['Abril',  90,       40,        30],
+    ['Maio',  95,       70,        80],
+    ['Junho',  87,       89,        70]
   ]);
 
   var options = {
