b00d44f461d46b1b7fc41f9b5beda8e9490bc0f2
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-05-27 12:51:51 -0300

    Terminado o gráfico de linhas dos dados de desempenho.

--- scripts/controleGraficoLinhas.js
+++ scripts/controleGraficoLinhas.js
@@ -4,14 +4,22 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Mes', 'Trem 1', 'Trem 2', 'Trem 3'],
-    ['Abril',  90,       40,        30],
-    ['Maio',  95,       70,        80],
-    ['Junho',  87,       89,        70]
+    ['Abril',  95,       90,        83],
+    ['Maio',  93,       96,        87],
+    ['Junho',  85,       89,        95]
   ]);
 
   var options = {
+    title: '% de viagens sem atraso',
+    titleTextStyle: {color: '#86bbd8'},
+    legend: 'none',
+    series: {
+      0: {color: '#a07932'},
+      1: {color: '#ffdd9f'},
+      2: {color: '#fdbe4d'}
+    },
+    backgroundColor: 'transparent',
   };
-
   var chart = new google.visualization.LineChart(document.getElementById('graficoDesempenho'));
 
   chart.draw(data, options);
