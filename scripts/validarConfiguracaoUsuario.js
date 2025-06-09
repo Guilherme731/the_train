7bc0c4171f8874525a0f2b5d9125716e509160f9
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-06-09 09:52:11 -0300

    Feita a responsividade do gerenciador de rotas

--- scripts/validarConfiguracaoUsuario.js
+++ scripts/validarConfiguracaoUsuario.js
@@ -1,13 +1,13 @@
 function validarConfigUser(){
-    const senha = document.getElementById("senha").value;
+    const senha = document.getElementById('senha').value;
     console.log(senha)
-    const Dia = document.getElementById("dataNascimentoDia").value;
+    const Dia = document.getElementById('dataNascimentoDia').value;
     console.log(Dia)
-    const Mes = document.getElementsByClassName("dataNascimentoMesAno").value;
+    const Mes = document.getElementsByClassName('dataNascimentoMes').value;
     console.log(Mes)
-    const Ano = document.getElementsByClassName("dataNascimentoMesAno").value;
+    const Ano = document.getElementsById('dataNascimentoAno').value;
     console.log(Ano)
-    const genero = document.getElementsByName("genero").value;
+    const genero = document.getElementsByName('genero').value;
     console.log(genero)
 
 
@@ -15,26 +15,36 @@ function validarConfigUser(){
     if(!senha){
         alert("Coloque Senha Para Continuar");
         return;
+    }else{
+        console.log(senha);
     }
 
     if(!Dia){
         alert("Coloque o Dia De Seu Nascimento Corretamente Para Continuar");
         return;        
+    }else{
+        console.log(Dia);
     }
 
     if(!Mes){
         alert("Coloque o Mês De Seu Nascimento Corretamente Para Continuar");
         return;        
+    }else{
+        console.log(Mes);
     }
 
     if(!Ano){
         alert("Coloque o Ano De Seu Nascimento Corretamente Para Continuar");
         return;        
+    }else{
+        console.log(Ano);
     }
 
     if(!genero){
         alert("Coloque o Seu Gênero Para Continuar");
         return;        
+    }else{
+        console.log(genero);
     }
 } 
 
