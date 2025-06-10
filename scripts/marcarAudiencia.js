d0bc0786d67401cbca7f8d8e48eab081cbf8b756
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-06-10 12:55:25 -0300

    quase finalizado da página marcar audiência.

---
+++ scripts/marcarAudiencia.js
@@ -0,0 +1,13 @@
+const adicionarButton = document.getElementById("adicionar");
+const cancelarButton = document.getElementById("cancelar");
+
+function adicionar(){
+    alert("Adicionado com sucesso!");
+}
+
+function cancelar(){
+    alert("Sua audiência foi cancelada");
+}
+
+
+
