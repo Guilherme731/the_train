3df6b67f2e902f157cc9d537f7a1bd71dd3223a7
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-06-12 10:10:32 -0300

    Modificacao dinamica do titulo do editor de rotas

--- scripts/editarRotas.js
+++ scripts/editarRotas.js
@@ -1,8 +1,9 @@
 
-
+const tituloPagina = document.getElementById('tituloPagina');
 let rota = sessionStorage.getItem('rotaParaEditar');
 if (rota) {
     visualizarRota(rota);
+    tituloPagina.innerText = 'Editando Rota ' + (parseInt(rota) + 1);
     } else {
     alert('Erro ao obter rota!');
 }
