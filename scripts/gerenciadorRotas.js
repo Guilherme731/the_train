451f49a32bf768a267d1981a42d2f6b554db580a
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-06-12 22:25:08 -0300

    Possibilidade de atribuicao de rotas aos trens

--- scripts/gerenciadorRotas.js
+++ scripts/gerenciadorRotas.js
@@ -1,2 +1,23 @@
 renderizarTrensMapa();
 escondeTrens();
+
+function aplicarRotas(){
+    alert('As mudanças não podem ser aplicadas pois não há banco de dados ainda!')
+}
+
+function criarRota(){
+    alert('Não é possível registrar uma nova rota porque não há banco de dados!')
+}
+
+function atualizarSelects(){
+    const selects = document.getElementsByClassName('selecionarRota');
+    const trem = JSON.parse(trensPosicoes);
+
+    let i = 0;
+    trem.trens.forEach(tremRota => {
+        selects[i].selectedIndex = tremRota[2];
+        i++;
+    });
+}
+
+atualizarSelects();
