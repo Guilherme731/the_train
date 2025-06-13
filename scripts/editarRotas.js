579ae1aced3ee2a3c0b283fa187c82b25343bb7d
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-06-12 22:42:12 -0300

    Modificar ordem das estacoes

--- scripts/editarRotas.js
+++ scripts/editarRotas.js
@@ -8,3 +8,22 @@ if (rota) {
     alert('Erro ao obter rota!');
 }
 
+function mover(botao, direcao) {
+      const item = botao.closest(".estacoes");
+      const container = document.getElementById("listaEstacoes");
+
+      if (direcao === "up" && item.previousElementSibling) {
+        container.insertBefore(item, item.previousElementSibling);
+      } else if (direcao === "down" && item.nextElementSibling) {
+        container.insertBefore(item.nextElementSibling, item);
+      }
+
+      exibirOrdem();
+    }
+
+    function exibirOrdem() {
+      const itens = document.querySelectorAll(".estacoes");
+      const ordem = Array.from(itens).map(el => el.dataset.id);
+      console.log("Ordem atual:", ordem);
+    }
+
