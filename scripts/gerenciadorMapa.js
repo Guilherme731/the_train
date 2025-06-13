4804f7ebcf3c322b2fbdc29e49e59cf0ed7b8007
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-06-12 21:32:58 -0300

    Criacao de funcao de adicionar quadrados (trens) no mapa

--- scripts/gerenciadorMapa.js
+++ scripts/gerenciadorMapa.js
@@ -1,12 +1,14 @@
 
+const mapa = document.getElementById('svgMapa');
+
 document.addEventListener('DOMContentLoaded', function () {
-    const linhasMapa = document.getElementsByClassName('linhaMapa');
-    for (let i = 0; i < linhasMapa.length; i++) {
-        linhasMapa[i].addEventListener('click', () => {
-            linhasMapa[i].classList.toggle('linhaMapaSelecionada');
-            // console.log(linhasMapa[i].id)
-        });
-    }
+    // const linhasMapa = document.getElementsByClassName('linhaMapa');
+    // for (let i = 0; i < linhasMapa.length; i++) {
+    //     linhasMapa[i].addEventListener('click', () => {
+    //         linhasMapa[i].classList.toggle('linhaMapaSelecionada');
+    //         // console.log(linhasMapa[i].id)
+    //     });
+    // }
 });
 
 function atualizaOpcoesMapa() {
@@ -59,4 +61,32 @@ function teste(){
 function abrirEdicaoRota(rota){
     sessionStorage.setItem('rotaParaEditar', rota);
     window.location.href = 'editarRotas.html';
+}
+
+function addQuadradoTrem(x, y, numero) {
+  const namespace = "http://www.w3.org/2000/svg";
+
+  // Cria o quadrado
+  const trem = document.createElementNS(namespace, "rect");
+  trem.setAttribute("x", x);
+  trem.setAttribute("y", y);
+  trem.setAttribute("width", "15");
+  trem.setAttribute("height", "15");
+  trem.setAttribute("fill", "#f26419");
+  trem.setAttribute("stroke", "black");
+  trem.setAttribute("stroke-width", "1");
+
+  // Cria o texto (centralizado)
+  const texto = document.createElementNS(namespace, "text");
+  texto.setAttribute("x", x + 7.5); // centro X do quadrado
+  texto.setAttribute("y", y + 7.5); // centro Y do quadrado
+  texto.setAttribute("text-anchor", "middle");
+  texto.setAttribute("dominant-baseline", "middle");
+  texto.setAttribute("font-size", "12");
+  texto.setAttribute("fill", "black");
+  texto.textContent = numero;
+
+  // Adiciona ao SVG
+  mapa.appendChild(trem);
+  mapa.appendChild(texto);
 }
