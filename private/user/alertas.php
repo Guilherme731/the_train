9796d88542339a1c53b67c61a1bd90597b7e67cc
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-09-16 12:49:54 -0300

    Linkado a página de alerta.

--- private/user/alertas.php
+++ private/user/alertas.php
@@ -37,10 +37,10 @@
             <div class="linhaAmarela"></div>
         </div>
         <div class="navbar">
-            <a href="../dashboard/dashboard.php"><img src="../../assets/icons/footer/dashboardIcone.png" alt="Dashboard"></a>
-            <a href="../gerenciadorDeRotas/gerenciarRotas.php"><img src="../../assets/icons/footer/rotasIcone.png" alt="Rotas"></a>
-            <a href="../manutencao/manutencao.php"><img src="../../assets/icons/footer/manutencaoIcone.png" alt="Manutenção"></a>
-            <a href="../relatorios/relatorios.php"><img src="../../assets/icons/footer/relatoriosIcone.png" alt="Relatórios"></a>
+            <a href="dashboard/dashboard.php"><img src="../../assets/icons/footer/dashboardIcone.png" alt="Dashboard"></a>
+            <a href="gerenciadorDeRotas/gerenciarRotas.php"><img src="../../assets/icons/footer/rotasIcone.png" alt="Rotas"></a>
+            <a href="manutencao/manutencao.php"><img src="../../assets/icons/footer/manutencaoIcone.png" alt="Manutenção"></a>
+            <a href="relatorios/relatorios.php"><img src="../../assets/icons/footer/relatoriosIcone.png" alt="Relatórios"></a>
         </div>
     </footer>
     <script src="../../scripts/alertas.js"></script>
