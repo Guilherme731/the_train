23ef3d9d52269cf72cf2d18a1a354b1338b713ee
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-09-25 09:28:46 -0300

    Colocado o botão para exibir a mensagem de alerta no dashboard.

--- private/user/dashboard/dashboard.php
+++ private/user/dashboard/dashboard.php
@@ -16,6 +16,12 @@ include '../../authGuard/authUsuario.php';
     <a href="../../../private/<?php if($_SESSION['tipo'] == 'admin'){echo 'admin/config/configAdmin.php';} else {echo 'user/config/configFuncionario.php';}?>"><img src="../../../assets/icons/header/engrenagemIcone.png" alt="Configurações"></a>
     <img src="../../../assets/logos/logoPequena.png" alt="Logo">
     <a href="../alertas.php"><img src="../../../assets/icons/header/sinoIcone.png" alt="Notificações"></a>
+    <button onclick="ExibirAlerta()" id="cancelar">Exibir Alerta</button>
+    <script>
+        function ExibirAlerta (){
+            alert("Dashboard em implementação!")
+        }
+    </script>
 </header>
 
     <section class="secaoInfo">
