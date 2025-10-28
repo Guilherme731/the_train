d2873e3084a2ba2e995b26a298f85acd8fdf3022
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-10-28 11:49:04 -0300

    Ajuste no banco de dados - concertado erro de sintaxe

--- private/scripsLDB/db.sql
+++ private/scripsLDB/db.sql
@@ -116,7 +116,7 @@ CREATE TABLE sensores_data(
     CONSTRAINT fk_sensores_id FOREIGN KEY (id_sensores) REFERENCES sensores(id)
 );
 
-INSERT INTO usuarios (`cargo`, `salario`, `genero`, `dataNascimento`, `senha`, `email`, `nome`, `cpf`, `tipo`, `imagemPerfil`, 'cep', 'rua', 'cidade', 'estado', 'numero') VALUES
+INSERT INTO usuarios (`cargo`, `salario`, `genero`, `dataNascimento`, `senha`, `email`, `nome`, `cpf`, `tipo`, `imagemPerfil`, `cep`, `rua`, `cidade`, `estado`, `numero`) VALUES
 ('Administrador', '5000', 'Feminino', '1997-04-27', '$2y$10$UQ8NEEGJMhR27KYLr6JwAOoa6YOXyrbF8jwiirn2QbgcG3eDmBJEC', 'admin@thetrain.com', 'Admin', '99999999999', 'admin', null, '89202205', 'Av. Getúlio Vargas', 'Joinville', 'Santa Catarina', 463),
 ('Operário', '3000', 'Masculino', '1999-05-27', '$2y$10$FXEtBMctNaZfGFhEG9cYgOcROkb0z.2xEayu9JKbZuWVg8L/0OisO', 'user@thetrain.com', 'Usuario', '99999999998', 'funcionario', 'perfilUsuario2.png', '81580290', 'Rua Tenente Coronel Benjamin Lage', 'Curitiba', 'Paraná', 52),
 ('Faxineiro', '2500', 'Masculino', '2008-03-23', '$2y$10$Rsbvwg2x95QebiQdyls6jeoRMhphc59FE4SpS.fONeu7Bn4L/wU4K', 'rodrigo@thetrain.com', 'Rodrigo', '77777777777', 'funcionario', 'perfilUsuario3.png', '89258110', 'R. José Pavanelo', 'Jaraguá do Sul', 'Santa Catarina', 205);
