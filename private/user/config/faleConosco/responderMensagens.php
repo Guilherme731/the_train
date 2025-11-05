90ef09c0142843a36cba510439f35d382ff22c40
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-11-05 09:44:59 -0300

    Feito o código inicial das novas abas de responder e ver mensagens da aba de fale conosco.

---
+++ private/user/config/faleConosco/responderMensagens.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responder Mensagens</title>
+</head>
+<body>
+    
+</body>
+</html>
