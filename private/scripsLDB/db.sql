e2cd46837c2bdfe35896d2e6e5a3f27d2bf6702a
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-11-06 09:30:18 -0300

    Populado a tabela de mensagens no banco de dados e feito o código php inicial da aba responderMensagens.

--- private/scripsLDB/db.sql
+++ private/scripsLDB/db.sql
@@ -189,3 +189,9 @@ VALUES
 (2, 3),
 (3, 3),
 (2, 1);
+
+INSERT INTO mensagens(tipo, id_remetente, id_destinatario, conteudo, data_envio)
+VALUES
+('duvida', 1, 1, 'Não entendi como funciona as notificações, como eu poderia criar uma nova?', '02/03/2025'),
+('reportarErro', 1, 2, 'Não estou conseguindo fazer login, mesmo meu email estando correto.', '04/06/2025'),
+('marcarAudiencia', 1, 3, 'Gostaria de marcar uma audiência para o dia 20/11/2025 para discutir os termos do projeto.', '06/11/2025');s
