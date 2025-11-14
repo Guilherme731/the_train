602c60456e66dac42e8a299852ea7ec5470d3edd
Rodrigo222520 <rodrigo_f_schulze@estudante.sesisenai.org.br>
2025-11-14 10:12:50 -0300

    Arrumado o codigoVerificacao.php.

--- private/user/config/verificacaoDuasEtapas/codigoVerificacao.php
+++ private/user/config/verificacaoDuasEtapas/codigoVerificacao.php
@@ -26,14 +26,19 @@ $numero6 = $_POST["numero6"] ?? "";
         $codigo_5 = intval($row['codigo_5']);
         $codigo_6 = intval($row['codigo_6']);
     }
-    echo "<div class='mensagemCodigo'> 
-        <p>O código enviado ao seu email é: $codigo_1 $codigo_2 $codigo_3 $codigo_4 $codigo_5 $codigo_6</p>
-        <a href='' class='fechar'>Fechar</a>
-        </div>";
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+    echo "<div class='felixCentro'> 
+         <div class='mensagemCodigo'> 
+                <p>O código enviado ao seu email é: $codigo_1 $codigo_2 $codigo_3 $codigo_4 $codigo_5 $codigo_6</p>
+                <a href='' class='fechar'>Fechar</a>
+                </div>
+        </div>";
+    
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){       
     if(isset($_POST['reenviar'])){
 
+        
         $numero1 = rand(0, 9);
         $numero2 = rand(0, 9);
         $numero3 = rand(0, 9);
@@ -46,87 +51,87 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $stmt->close();
-    } else if(isset($_POST['verificar'])){
-    $temFTA = $_POST["temFTA"] ?? "";
-    $boolean = 1;
-    if($numero1 == $codigo_1 && $numero2 == $codigo_2 && $numero3 == $codigo_3 && $numero4 == $codigo_4 && $numero5 == $codigo_5 && $numero6 == $codigo_6){
-        $stmt = $conn->prepare("UPDATE usuarios SET temTFA=? WHERE id=?");
-        $stmt->bind_param("ii", $boolean, $id);
-        $stmt->execute();
-        $stmt->close();
-        echo "<div class='mensagemCodigo'> 
-        <p>Código de verificação de duas etapas aplicado com sucesso.</p>
-        <a href='' class='fechar'>Fechar</a>
-            </div>";
-    
-    }else{
-        echo "<div class='mensagemErro'> 
-        <p>Código incorreto.</p>
-        <a href='' class='fechar'>Fechar</a>
-            </div>";
-    }
-    }
 
 
+    } elseif(isset($_POST['verificar'])) {
+        $temFTA = $_POST["temFTA"] ?? "";
+        $boolean = 1;
+        if($numero1 == $codigo_1 && $numero2 == $codigo_2 && $numero3 == $codigo_3 && $numero4 == $codigo_4 && $numero5 == $codigo_5 && $numero6 == $codigo_6){
+            $stmt = $conn->prepare("UPDATE usuarios SET temTFA=? WHERE id=?");
+            $stmt->bind_param("ii", $boolean, $id);
+            $stmt->execute();
+            $stmt->close();
+            echo "<div class='mensagemCodigo'> 
+            <p>Código de verificação de duas etapas aplicado com sucesso.</p>
+            <a href='' class='fechar'>Fechar</a>
+                </div>";
+        }else{
+            echo "<div class='mensagemErro'> 
+            <p>Código incorreto.</p>
+            <a href='' class='fechar'>Fechar</a>
+                </div>";
+        }
+    }
 }
 
 ?>
 
-<!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../../../../assets/logos/logoPequena.png">
-    <script src="../../../../scripts/botoesMenus.js"></script>
-    <script src="../../../../scripts/configFuncionarioSalvarInformacoes.js"></script>
-
     <link rel="stylesheet" href="../../../../style/style.css">
-
-    <title>Verificação de duas etapas</title>
+    <title>Verificação de 2 etapas</title>
 </head>
+
 <body>
-    <header class="headerAzulVoltar">
-        <a href="../../config/configFuncionario.php">
-            <img src="../../../../assets/icons/header/setaEsquerda.png" alt="Seta" onclick="voltarPagina()">
-        </a>
+    <header class="headerAzulLogo">
+        <img id="ajusteImagem" src="../../../../assets/logos/logoPequena.png" alt="Logo Pequena">
     </header>
-
+    <a href="../../../../public/recuperarSenha2.php">
+        <img src="../../../../assets/icons/header/setaEsquerdaClara.PNG" alt="Seta" onclick="voltarPagina()">
+    </a>
+    <div class="tituloAzul">
+        <h1>Verificação de 2 etapas</h1>
+        <br>
+    </div>
     <main>
-    <div class="flexCentral textoCentral marginTop-100">
-        <img id="iconeVerificacao2Etapas" src="../../../../assets/icons/config/verificacaoDuasEtapas/verificacao2EtapasIconeLaranja.png" alt="Icone de verificação de 2 etapas Laranja">
-    </div>
-
-    <div class="flexCentro">
-        <form action="" method="POST">
-        <input name="numero1" class="campoNumeroVerificacao2Etapas" type="number" max="9" min="0" required> 
-
-        <input name="numero2" class="campoNumeroVerificacao2Etapas" type="number" max="9" min="0" required>
-    
-        <input name="numero3" class="campoNumeroVerificacao2Etapas" type="number" max="9" min="0" required>
-    
-        <input name="numero4" class="campoNumeroVerificacao2Etapas" type="number" max="9" min="0" required>
-    
-        <input name="numero5" class="campoNumeroVerificacao2Etapas" type="number" max="9" min="0" required>
-    
-        <input name="numero6" class="campoNumeroVerificacao2Etapas" type="number" max="9" min="0" required>
-
         <br>
         <br>
-
-        <div class="flexCentro">
-        <button type="submit" class="botaoEnviar" name="verificar">Ativar verificação</button>
+        <br>
+        <div class="container">
+            <form method="POST" action="">
+                <div class="grupoInputs">
+                    <input type="number" name="numero1" required>
+                    <input type="number" name="numero2" required>
+                    <input type="number" name="numero3" required>
+                    <input type="number" name="numero4" required>
+                    <input type="number" name="numero5" required>
+                    <input type="number" name="numero6" required>
+                </div>
+                <h2 class="tituloAzul">
+                    Um código de verificação foi enviado para o seu email. Insira o código para continuar.
+                </h2>
+                
+                <div class="flexCentro">
+                    <button class="ativo" type="submit" name="verificar">Verificar</button>
+                   
+                </div>
+            </form>
+              <form action="" method="post">
+                <button class="ativo" type="submit" name="reenviar">Reenviar código</button>
+              </form>
         </div>
-        </form>
+      
+      
+    </main>
+    <div class="espacoFooterAzul"></div>
+    <footer class="footerAzulArredondado">
 
-        <form action="" method="post">
-        <br>
-        <br>
-        <br>
-        <br>
-        <button class="botaoEnviar" type="submit" name="reenviar">Reenviar código</button></form>
-    </div>
-       <script>
+    </footer>
+    <script>
     document.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('.fechar').forEach(function(btn) {
             btn.addEventListener('click', function(e) {
@@ -137,14 +142,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         });
     });
     </script>
-
-    <p id="textoVerificacao2EtapasConfirmacao">Um código de verificação foi enviado para o seu email. Insira o código para continuar.</p>
-    </main>
-
-    <div class="espacoFooterAzulLogo"></div>
-
-    <footer class="footerAzulLogo">
-        <img src="../../../../assets/logos/logoCompleta.png" alt="Logo">
-    </footer>
 </body>
+
 </html>
