55db4a5e2e8fe48ef3f0483f8e4514a6ab108d5a
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-11-25 22:57:20 -0300

    sql atualiza com base no mqtt topicos S1

--- mqtt_server/server.js
+++ mqtt_server/server.js
@@ -14,12 +14,12 @@ var client = mqtt.connect(options);
 
 client.on("connect", () => {
     console.log("Conectado ao MQTT");
-    client.subscribe("teste");
+    client.subscribe(["S1/Iluminacao", "S1/Temp", "S1/Presenca"]);
 });
 
 client.on("message", async (topic, message) => {
     const payload = message.toString();
-    console.log("Recebido:", payload);
+    console.log("Recebido t:", topic, " m:", payload);
 
     const conn = await mysql.createConnection({
         host: "localhost",
@@ -28,9 +28,30 @@ client.on("message", async (topic, message) => {
         database: "the_train_db"
     });
 
-    if (payload === "STATUS:OK") {
-        await conn.execute("UPDATE trens SET quantidadePassageiros=35 WHERE id=1");
+    if(topic == "S1/Iluminacao"){
+        if (payload == "1") {
+            await conn.execute("UPDATE estacoes SET estaChovendo=1");
+        }else if(payload == "0"){
+            await conn.execute("UPDATE estacoes SET estaChovendo=0");
+        }
+    }else if(topic == "S1/Temp"){
+        await conn.execute("UPDATE estacoes SET temperatura=" + payload);
     }
+    else if(topic == "S1/Presenca" && payload == "1"){
+        const [rows] = await conn.execute('SELECT ordemRota, idEstacao FROM trens WHERE id = 1', [1]);
+        let currentOrdem = rows.length ? rows[0].ordemRota : 0;
+        //let antigaEstacao = rows.length ? rows[0].ordemRota : 0;
+        if(currentOrdem >=6){
+            currentOrdem = 0;
+        }
+        const data = new Date();
+        await conn.execute("UPDATE trens SET quantidadePassageiros=" + numeroInteiroAleatorio(1,150) + ", parado=0, idEstacao=1, horaSaida='"+ data.getHours() +":" + data.getMinutes() + ":" + data.getSeconds() +"', ordemRota=" + (currentOrdem + 1) + " WHERE id=1");
+    }
+    //await conn.execute("UPDATE trens SET quantidadePassageiros=35 WHERE id=1");
 
     await conn.end();
 });
+
+function numeroInteiroAleatorio(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
